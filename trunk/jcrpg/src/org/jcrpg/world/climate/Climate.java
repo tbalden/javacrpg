4ba272f6bff1b216a42f59c0ab19f8f9b34e90db
skrionius <skrionius@d65a9143-dc31-0410-b20e-ae5b3973b0f5>
2007-06-20 18:49:46 +0000

    license

--- trunk/jcrpg/src/org/jcrpg/world/climate/Climate.java
+++ trunk/jcrpg/src/org/jcrpg/world/climate/Climate.java
@@ -19,11 +19,8 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
+package org.jcrpg.world.climate;
 
-package org.jcrpg.world.place;
+public class Climate {
 
-public interface Surface {
-	
-	public SurfaceHeightAndType getPoint(int x, int z);
-	
 }
