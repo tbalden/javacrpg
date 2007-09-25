17587e012530d6be7d17858d90f5cadfa2bfa962
Pál Zoltán Illés <1277993+tbalden@users.noreply.github.com>
2007-09-25 13:49:39 +0000

    update to geomBatch

--- jcrpg/src/org/jcrpg/threed/jme/geometryinstancing/GeometryBatchSpatialInstance.java
+++ jcrpg/src/org/jcrpg/threed/jme/geometryinstancing/GeometryBatchSpatialInstance.java
@@ -1,35 +1,3 @@
-/*
- * Copyright (c) 2003-2007 jMonkeyEngine
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- * * Redistributions of source code must retain the above copyright
- *   notice, this list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright
- *   notice, this list of conditions and the following disclaimer in the
- *   documentation and/or other materials provided with the distribution.
- *
- * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
- *   may be used to endorse or promote products derived from this software
- *   without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.jcrpg.threed.jme.geometryinstancing;
 
 import java.nio.Buffer;
@@ -39,24 +7,24 @@ import java.nio.IntBuffer;
 import org.jcrpg.threed.jme.geometryinstancing.instance.GeometryInstance;
 
 import com.jme.math.Vector3f;
-import com.jme.scene.TriMesh;
+import com.jme.scene.Geometry;
+import com.jme.scene.batch.GeomBatch;
 import com.jme.scene.batch.TriangleBatch;
 import com.jme.util.geom.BufferUtils;
 
 /**
- * <code>GeometryBatchInstance</code> uses a <code>GeometryBatchInstanceAttributes</code>
- * to define an instance of object in world space. Uses TriangleBatch as source
- * data for the instance, instead of GeomBatch which does not have an index
- * buffer.
+ * <code>GeometryBatchSpatialInstance</code> uses <code>GeometryBatchInstanceAttributes</code>
+ * to define an instance of object in world space. Uses Geometry as source
+ * data for the instance.
  *
  * @author Patrik Lindegrén
  */
 public class GeometryBatchSpatialInstance<A extends GeometryBatchInstanceAttributes> extends GeometryInstance<A> {
-	public TriMesh mesh;
+	public Geometry mesh;
 	protected AABB modelBound;
 	private FloatBuffer tempVertBuf;
 	
-	protected TriangleBatch instanceBatch = null;
+	protected GeomBatch instanceBatch = null;
 	
 	protected boolean transformed = false;
 	private boolean updateVerts = false;
@@ -64,7 +32,7 @@ public class GeometryBatchSpatialInstance<A extends GeometryBatchInstanceAttribu
 	
 	protected boolean forceUpdate = false;
 	
-	public GeometryBatchSpatialInstance(TriMesh mesh, A attributes) {
+	public GeometryBatchSpatialInstance(Geometry mesh, A attributes) {
         super(attributes);
         this.mesh = mesh;
         instanceBatch = mesh.getBatch(0);
@@ -126,7 +94,7 @@ public class GeometryBatchSpatialInstance<A extends GeometryBatchInstanceAttribu
      *
      * @param batch
      */
-    public void commit(TriangleBatch batch, boolean force) {
+    public void commit(GeomBatch batch, boolean force) {
         int indexStart = commitVertices(batch);
         commitIndices(batch, indexStart);
         commitNormals(batch);
@@ -164,7 +132,7 @@ public class GeometryBatchSpatialInstance<A extends GeometryBatchInstanceAttribu
         return indexStart;
     }
     
-    protected int commitVertices(TriangleBatch batch) {
+    protected int commitVertices(GeomBatch batch) {
     	// Vertex buffer
        	if (updateVerts) {
             updateVerts = false;
@@ -174,20 +142,23 @@ public class GeometryBatchSpatialInstance<A extends GeometryBatchInstanceAttribu
         return 0;
     }
     
-    protected void commitIndices(TriangleBatch batch, int indexStart) {
+    protected void commitIndices(GeomBatch batch, int indexStart) {
+    	if (!(instanceBatch instanceof TriangleBatch && batch instanceof TriangleBatch)) {
+    		return;
+    	}
     	// Index buffer        
     	if (updateIndices) {
-    		IntBuffer indexBufSrc = instanceBatch.getIndexBuffer();
-            IntBuffer indexBufDst = batch.getIndexBuffer();
+    		IntBuffer indexBufSrc = ((TriangleBatch)instanceBatch).getIndexBuffer();
+            IntBuffer indexBufDst = ((TriangleBatch)batch).getIndexBuffer();
             if (indexBufSrc != null && indexBufDst != null) {
         		updateIndices = false;
 	            indexBufSrc.rewind();
-	            for (int i = 0; i < instanceBatch.getMaxIndex(); i++) {
+	            for (int i = 0; i < getNumIndices(); i++) {
 	                indexBufDst.put(indexStart + indexBufSrc.get());
 	            }
         	} 
         } else {
-    		skipBuffer(instanceBatch.getIndexBuffer(), batch.getIndexBuffer());
+    		skipBuffer(((TriangleBatch)instanceBatch).getIndexBuffer(), ((TriangleBatch)batch).getIndexBuffer());
     	}
     }
     
@@ -207,7 +178,7 @@ public class GeometryBatchSpatialInstance<A extends GeometryBatchInstanceAttribu
         }
     }
     
-    protected void commitNormals(TriangleBatch batch) {
+    protected void commitNormals(GeomBatch batch) {
     	// Normal buffer
     	if ((forceUpdate || transformed) && attributes.isVisible()) {
     		commitNormals(instanceBatch.getNormalBuffer(), batch.getNormalBuffer());
@@ -216,7 +187,7 @@ public class GeometryBatchSpatialInstance<A extends GeometryBatchInstanceAttribu
     	}
     }
         
-    protected void commitTextureCoords(TriangleBatch batch) {
+    protected void commitTextureCoords(GeomBatch batch) {
     	// Texture buffers
         for (int i = 0; i < 8; i++) {
             FloatBuffer texBufSrc = instanceBatch.getTextureBuffer(i);
@@ -232,7 +203,7 @@ public class GeometryBatchSpatialInstance<A extends GeometryBatchInstanceAttribu
         }
     }
     
-	protected void commitColors(TriangleBatch batch) {
+	protected void commitColors(GeomBatch batch) {
 	 // Color buffer
 	    FloatBuffer colorBufSrc = instanceBatch.getColorBuffer();
 	    FloatBuffer colorBufDst = batch.getColorBuffer();
@@ -267,7 +238,10 @@ public class GeometryBatchSpatialInstance<A extends GeometryBatchInstanceAttribu
         if (instanceBatch == null) {
             return 0;
         }
-        return instanceBatch.getMaxIndex();
+        if ((instanceBatch.getType() & TriangleBatch.TRIANGLEBATCH)>0) {
+        	return ((TriangleBatch)instanceBatch).getMaxIndex();        	
+        }
+        return 0;
     }
 
     public int getNumVerts() {
