7c9b8a3ead1feee1f79584ffb1bd3eb5febd75db
Pál Zoltán Illés <1277993+tbalden@users.noreply.github.com>
2007-09-26 19:35:40 +0000

    To GPLv3

--- jcrpg/src/org/jcrpg/world/ai/flora/impl/MountainFloraGenerator.java
+++ jcrpg/src/org/jcrpg/world/ai/flora/impl/MountainFloraGenerator.java
@@ -1,44 +1,31 @@
 /*
- * Java Classic RPG
- * Copyright 2007, JCRPG Team, and individual contributors as indicated
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+ *  This file is part of JavaCRPG.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ *  JavaCRPG is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ *  JavaCRPG is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.jcrpg.world.ai.flora.impl;
 
 import org.jcrpg.world.ai.flora.FloraListElement;
 import org.jcrpg.world.ai.flora.ground.Grass;
-import org.jcrpg.world.ai.flora.ground.JungleGround;
-import org.jcrpg.world.ai.flora.ground.Snow;
 import org.jcrpg.world.ai.flora.middle.deciduous.GreenBush;
-import org.jcrpg.world.ai.flora.middle.succulent.GreenFern;
-import org.jcrpg.world.ai.flora.tree.deciduous.Acacia;
 import org.jcrpg.world.ai.flora.tree.deciduous.CherryTree;
 import org.jcrpg.world.ai.flora.tree.deciduous.OakTree;
-import org.jcrpg.world.ai.flora.tree.palm.CoconutTree;
-import org.jcrpg.world.ai.flora.tree.palm.JunglePalmTrees;
 import org.jcrpg.world.ai.flora.tree.pine.GreatPineTree;
 import org.jcrpg.world.ai.flora.tree.pine.GreenPineTree;
 import org.jcrpg.world.climate.ClimateLevel;
-import org.jcrpg.world.climate.impl.arctic.Arctic;
 import org.jcrpg.world.climate.impl.continental.Continental;
-import org.jcrpg.world.climate.impl.tropical.Tropical;
 
 public class MountainFloraGenerator extends BaseFloraGenerator{
 
