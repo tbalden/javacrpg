0f1472029280f0ee6122a35f40b47c48bff9aa0f
Pál Zoltán Illés <1277993+tbalden@users.noreply.github.com>
2007-10-18 10:54:10 +0000

    lake v1 complete!

--- jcrpg/src/org/jcrpg/world/place/water/Lake.java
+++ jcrpg/src/org/jcrpg/world/place/water/Lake.java
@@ -20,6 +20,8 @@ package org.jcrpg.world.place.water;
 
 import org.jcrpg.space.Cube;
 import org.jcrpg.space.Side;
+import org.jcrpg.space.sidetype.GroundSubType;
+import org.jcrpg.space.sidetype.NotPassable;
 import org.jcrpg.space.sidetype.Swimming;
 import org.jcrpg.util.HashUtil;
 import org.jcrpg.world.place.BoundaryUtils;
@@ -34,10 +36,21 @@ public class Lake extends Water {
 
 	public static final String TYPE_LAKE = "LAKE";
 	public static final Swimming SUBTYPE_WATER = new Swimming(TYPE_LAKE+"_WATER");
+	public static final NotPassable SUBTYPE_ROCKSIDE = new NotPassable(TYPE_LAKE+"_ROCKSIDE");
+	public static final GroundSubType SUBTYPE_ROCKBOTTOM = new GroundSubType(TYPE_LAKE+"_ROCKBOTTOM");
+	public static final Swimming SUBTYPE_WATER_EMPTY = new Swimming(TYPE_LAKE+"_WATER_EMPTY");
 
 	static Side[] WATER = {new Side(TYPE_LAKE,SUBTYPE_WATER)};
+	static Side[] ROCKSIDE = {new Side(TYPE_LAKE,SUBTYPE_ROCKSIDE)};
+	static Side[] ROCKBOTTOM = {new Side(TYPE_LAKE,SUBTYPE_ROCKBOTTOM)};
+	static Side[] WATER_EMPTY = {new Side(TYPE_LAKE,SUBTYPE_WATER_EMPTY)};
 
 	static Side[][] LAKE_WATER = new Side[][] { null, null, null,null,null,WATER };
+	static Side[][] LAKE_ROCKSIDE_NORTH = new Side[][] { ROCKSIDE, null, null,null,null,WATER_EMPTY };
+	static Side[][] LAKE_ROCKSIDE_SOUTH = new Side[][] { null, null, ROCKSIDE,null,null,WATER_EMPTY };
+	static Side[][] LAKE_ROCKSIDE_EAST = new Side[][] { null, ROCKSIDE, null,null,null,WATER_EMPTY };
+	static Side[][] LAKE_ROCKSIDE_WEST = new Side[][] { null, null, null,ROCKSIDE,null,WATER_EMPTY };
+	static Side[][] LAKE_ROCKSIDE_BOTTOM = new Side[][] { null, null, null, null, null,ROCKBOTTOM };
 
 	int magnification, sizeX, sizeY, sizeZ, origoX, origoY, origoZ;
 
@@ -80,21 +93,59 @@ public class Lake extends Water {
 	@Override
 	public Cube getWaterCube(int x, int y, int z, Cube geoCube,
 			SurfaceHeightAndType surface) {
-		if (y==worldGroundLevel) 
+		if (y==worldGroundLevel && !noWaterInTheBed) 
 		{
 			return new Cube (this,LAKE_WATER,x,y,z,SurfaceHeightAndType.NOT_STEEP);
 		} else
 		{
-			if (surface.surfaceY-y > depth)
+			if (worldGroundLevel-y > depth || y==worldGroundLevel)
 			{
 				// below bottom, return empty
 			} else 
 			{
 				boolean bottom = false;
-				if (surface.surfaceY-y == depth)
+				if (worldGroundLevel-y == depth)
 				{
 					bottom = true;
 				}
+				boolean northRock = false;
+				boolean southRock = false;
+				boolean westRock = false;
+				boolean eastRock = false;
+				Cube c = null;
+				if (!bottom)
+				{
+					c = new Cube (this,EMPTY,x,y,z,surface.steepDirection);
+				} else
+				{
+					c = new Cube (this,LAKE_ROCKSIDE_BOTTOM,x,y,z,surface.steepDirection);
+				}
+				// direction rockside tests
+				if (!isWaterPoint(x+1, y, z))
+				{
+					eastRock = true;
+					Cube c2 = new Cube (this,LAKE_ROCKSIDE_EAST,x,y,z,surface.steepDirection);
+					c = new Cube(c,c2,x,y,z,surface.steepDirection);
+				}
+				if (!isWaterPoint(x-1, y, z))
+				{
+					westRock = true;
+					Cube c2 = new Cube (this,LAKE_ROCKSIDE_WEST,x,y,z,surface.steepDirection);
+					c = new Cube(c,c2,x,y,z,surface.steepDirection);
+				}
+				if (!isWaterPoint(x, y, z+1))
+				{
+					northRock = true;
+					Cube c2 = new Cube (this,LAKE_ROCKSIDE_NORTH,x,y,z,surface.steepDirection);
+					c = new Cube(c,c2,x,y,z,surface.steepDirection);
+				}
+				if (!isWaterPoint(x, y, z-1))
+				{
+					southRock = true;
+					Cube c2 = new Cube (this,LAKE_ROCKSIDE_SOUTH,x,y,z,surface.steepDirection);
+					c = new Cube(c,c2,x,y,z,surface.steepDirection);
+				}
+				return c;
 			}
 		}
 		return new Cube (this,EMPTY,x,y,z,SurfaceHeightAndType.NOT_STEEP);
@@ -108,7 +159,7 @@ public class Lake extends Water {
 		int localY = y-worldGroundLevel;
 		int localZ = z-origoZ;
 		temp.set(localX, localZ, 0);
-		if (y==worldGroundLevel) 
+		if (worldGroundLevel-y <= depth && worldGroundLevel-y>=0) 
 		{
 			if (temp.distance(center)<realSizeX/2)
 			{
