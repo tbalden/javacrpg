412a0db8bc8223b0b719f1310197d6bda13f12be
skrionius <skrionius@d65a9143-dc31-0410-b20e-ae5b3973b0f5>
2007-11-26 15:48:51 +0000

    generator 1

---
+++ trunk/jcrpg/src/org/jcrpg/world/generator/WorldParams.java
@@ -0,0 +1,53 @@
+/*
+ *  This file is part of JavaCRPG.
+ *  Copyright (C) 2007 Illes Pal Zoltan
+ *
+ *  JavaCRPG is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JavaCRPG is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
+package org.jcrpg.world.generator;
+
+public class WorldParams {
+
+	public int magnification = 100; 
+	public int sizeX = 1;
+	public int sizeY = 1;
+	public int sizeZ = 1;
+	
+	/**
+	 * 0-10 - bigger denser
+	 */
+	public int landDensity = 1;
+	/**
+	 * 0-10 - bigger bigger parts
+	 */
+	public int landMass = 1;
+	
+	public int randomSeed = 0;
+
+	public WorldParams(int magnification, int sizeX, int sizeY, int sizeZ, int landDensity, int landMass, int randomSeed) {
+		super();
+		this.magnification = magnification;
+		this.sizeX = sizeX;
+		this.sizeY = sizeY;
+		this.sizeZ = sizeZ;
+		this.landDensity = landDensity;
+		this.landMass = landMass;
+		this.randomSeed = randomSeed;
+	}
+
+	
+	
+	
+}
