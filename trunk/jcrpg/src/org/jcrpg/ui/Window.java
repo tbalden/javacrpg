527cae05c42c5c2a7f181b8d2014769ab3520c4d
skrionius <skrionius@d65a9143-dc31-0410-b20e-ae5b3973b0f5>
2008-02-05 09:56:26 +0000

    ui things

--- trunk/jcrpg/src/org/jcrpg/ui/Window.java
+++ trunk/jcrpg/src/org/jcrpg/ui/Window.java
@@ -18,10 +18,19 @@
 
 package org.jcrpg.ui;
 
+import java.io.File;
+
 import org.jcrpg.threed.J3DCore;
 import org.jcrpg.threed.input.ClassicKeyboardLookHandler;
 
+import com.jme.image.Image;
+import com.jme.image.Texture;
+import com.jme.math.Vector3f;
+import com.jme.renderer.Renderer;
 import com.jme.scene.Node;
+import com.jme.scene.shape.Quad;
+import com.jme.scene.state.TextureState;
+import com.jme.util.TextureManager;
 
 public abstract class Window {
 
@@ -40,12 +49,12 @@ public abstract class Window {
 		windowNode = new Node("windowNode");
 
 	}
-	
+	boolean storedEnginePauseState = false; 
 	public synchronized void toggle()
 	{
 		if (visible)
 		{
-			core.engine.setPause(false);
+			core.engine.setPause(storedEnginePauseState);
 			windowCounter--;
 			if (windowCounter==0) ((ClassicKeyboardLookHandler)core.getInputHandler().getFromAttachedHandlers(0)).unlockSecondaryHandling();
 			hide();
@@ -53,10 +62,32 @@ public abstract class Window {
 		{
 			windowCounter++;
 			((ClassicKeyboardLookHandler)core.getInputHandler().getFromAttachedHandlers(0)).lockSecondaryHandling();
+			storedEnginePauseState = core.engine.isPause();
 			core.engine.setPause(true);
 			show();
 		}
 		visible=!visible;
+	}
+	
+	public Quad loadImageToQuad(String fileName, float sizeX, float sizeY,
+			float posX, float posY) throws Exception {
+		Quad hudQuad = new Quad(fileName, sizeX, sizeY);
+		hudQuad.setRenderQueueMode(Renderer.QUEUE_ORTHO);
+
+		hudQuad.setLocalTranslation(new Vector3f(posX, posY, 0));
+
+		Image hudImage = TextureManager.loadImage(new File(fileName).toURI()
+				.toURL(), true);
+
+		TextureState state = core.getDisplay().getRenderer()
+				.createTextureState();
+		Texture texture = new Texture();
+		texture.setImage(hudImage);
+		state.setTexture(texture);
+		hudQuad.setRenderState(state);
+
+		return hudQuad;
+
 	}
 	
 	public abstract void hide();
