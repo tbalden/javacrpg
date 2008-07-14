e6437fe5d44cba321ca4775c75cee26216520a64
Pál Zoltán Illés <1277993+tbalden@users.noreply.github.com>
2008-07-14 20:59:21 +0000

    climate dependent battleground

--- jcrpg/src/org/jcrpg/threed/moving/J3DEncounterEngine.java
+++ jcrpg/src/org/jcrpg/threed/moving/J3DEncounterEngine.java
@@ -17,14 +17,16 @@
  */ 
 package org.jcrpg.threed.moving;
 
+import java.lang.reflect.Constructor;
 import java.util.HashMap;
 
+import org.jcrpg.space.Cube;
 import org.jcrpg.threed.J3DCore;
 import org.jcrpg.threed.standing.J3DStandingEngine;
 import org.jcrpg.world.ai.flora.impl.BaseFloraContainer;
 import org.jcrpg.world.climate.Climate;
 import org.jcrpg.world.climate.ClimateBelt;
-import org.jcrpg.world.climate.impl.tropical.Tropical;
+import org.jcrpg.world.climate.CubeClimateConditions;
 import org.jcrpg.world.place.BoundaryUtils;
 import org.jcrpg.world.place.World;
 import org.jcrpg.world.place.geography.Plain;
@@ -34,8 +36,7 @@ import org.jcrpg.world.place.orbiter.sun.SimpleSun;
 
 public class J3DEncounterEngine extends J3DStandingEngine {
 	
-	HashMap<String, World> encounterGroundWorlds = new HashMap<String, World>();
-	World debugWorld = null;
+	World fallbackWorld = null;
 	public J3DEncounterEngine(J3DCore core) {
 		super(core);
 		extRootNode = core.encounterExtRootNode;
@@ -43,49 +44,68 @@ public class J3DEncounterEngine extends J3DStandingEngine {
 		renderedArea = core.renderedEncounterArea;
 		optimizeAngle = false;
 		
-		try 
-		{
-			World baseWorld = new World("encounterGround",null,100,1,1,1);
-			baseWorld.engine = core.gameState.engine;
-			int wMag = 100;
-			int wX = 1;
-			int wY = 1;
-			//int wZ = 1;
-			
-			// FLORA
-			baseWorld.setFloraContainer(new BaseFloraContainer());
-			
-			// ORBITERS
-			WorldOrbiterHandler woh = new WorldOrbiterHandler();
-			woh.addOrbiter("sun", new SimpleSun("SUN"));
-			woh.addOrbiter("moon", new SimpleMoon("moon"));
-			baseWorld.setOrbiterHandler(woh);
-			
-			// CLIMATE
-			// TODO -> do not duplicate climate belts! shrinkToWorld must be reconsidered!! turn direction when crossing the world limit
-			Climate climate = new Climate("climate",baseWorld);
-			baseWorld.setClimate(climate);
-			
-			ClimateBelt belt = new Tropical("tropical",climate);
-			belt.setBoundaries(BoundaryUtils.createCubicBoundaries(wMag, wX, wY, 1, 0, 0, 0));
-			climate.belts.put(belt.id, belt);
-			
-			Plain plain = new Plain("plain1",baseWorld,null,0,100,1,1,1,0,0,0,true);
-			baseWorld.addGeography(plain);
-			encounterGroundWorlds.put("BASE", baseWorld);
-			debugWorld = baseWorld;
-		} catch (Exception ex)
-		{	
-			ex.printStackTrace();
-		}
+	}
+	HashMap<String, World> encounterGroundWorlds = new HashMap<String, World>();
+	
+	public World getEncounterGroundWorld(Cube c, CubeClimateConditions ccc, String specialType) throws Exception
+	{
+		World baseWorld = new World("encounterGround",null,100,1,1,1);
+		baseWorld.engine = core.gameState.engine;
+		int wMag = 100;
+		int wX = 1;
+		int wY = 1;
+		//int wZ = 1;
+		
+		// FLORA
+		baseWorld.setFloraContainer(new BaseFloraContainer());
+		
+		// ORBITERS
+		WorldOrbiterHandler woh = new WorldOrbiterHandler();
+		woh.addOrbiter("sun", new SimpleSun("SUN"));
+		woh.addOrbiter("moon", new SimpleMoon("moon"));
+		baseWorld.setOrbiterHandler(woh);
+		
+		Climate climate = new Climate("climate",baseWorld);
+		baseWorld.setClimate(climate);
+		
+		Class beltClass = ccc.getBelt().getClass();
+		Constructor<?> constructor = (Constructor<?>)beltClass.getConstructors()[0];
+		ClimateBelt belt = (ClimateBelt)constructor.newInstance("encWorld "+beltClass,climate);
+		belt.setBoundaries(BoundaryUtils.createCubicBoundaries(wMag, wX, wY, 1, 0, 0, 0));
+		climate.belts.put(belt.id, belt);
+		
+		Plain plain = new Plain("plain1",baseWorld,null,0,100,1,1,1,0,0,0,true);
+		baseWorld.addGeography(plain);
+		encounterGroundWorlds.put("BASE", baseWorld);
+		fallbackWorld = baseWorld;
+		return baseWorld;
 	}
 	
 	public String lastType = "________";
-	public void renderToEncounterWorld(String type)
+	public void renderToEncounterWorld(int worldX, int worldY, int worldZ, World realWorld, String specialType)
 	{
+		Cube c = realWorld.getCube(-1, worldX, worldY, worldZ, false);
+		CubeClimateConditions ccc = realWorld.getCubeClimateConditions(world.engine.getWorldMeanTime(), worldX, worldY, worldZ, c.internalCube);
+		
+		String type = ccc.getPartialBeltLevelKey()+"__"+specialType; 
+		
 		if (type == null && lastType == null || type.equals(lastType)) return;
 		lastType = type;
-		world = debugWorld;
+		
+		World w = encounterGroundWorlds.get(type);
+		if (w==null)
+		{ 
+			try {
+				w = getEncounterGroundWorld(c, ccc, specialType);
+			} catch (Exception ex)
+			{
+				ex.printStackTrace();
+				w = fallbackWorld;
+			}
+			encounterGroundWorlds.put(type, w);
+		}
+		
+		world = w;
 		rerender = true;
 		renderToViewPort();
 		rerender = false;
