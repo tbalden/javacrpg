9adba5b7907796a50e39f046df6e632239f37487
Pál Zoltán Illés <1277993+tbalden@users.noreply.github.com>
2008-07-23 15:26:12 +0000

    fixing encounter Y pos, populatePopulation

--- jcrpg/src/org/jcrpg/world/place/economic/SimpleDistrict.java
+++ jcrpg/src/org/jcrpg/world/place/economic/SimpleDistrict.java
@@ -4,7 +4,6 @@ import org.jcrpg.world.ai.EntityInstance;
 import org.jcrpg.world.place.Geography;
 import org.jcrpg.world.place.PlaceLocator;
 import org.jcrpg.world.place.World;
-import org.jcrpg.world.place.economic.infrastructure.DefaultInfrastructure;
 import org.jcrpg.world.place.economic.infrastructure.GrownInfrastructure;
 
 public class SimpleDistrict extends Population {
@@ -14,15 +13,15 @@ public class SimpleDistrict extends Population {
 	}
 
 	public SimpleDistrict(String id, Geography soilGeo, World parent,
-			PlaceLocator loc, EntityInstance owner, int blockStartX, int blockStartZ) {
-		super(id, soilGeo, parent, loc, owner,blockStartX,blockStartZ);
+			PlaceLocator loc, EntityInstance owner, int blockStartX, int blockStartZ, int centerX, int centerZ) {
+		super(id, soilGeo, parent, loc, owner,blockStartX,blockStartZ,centerX,centerZ);
 		infrastructure = new GrownInfrastructure(this);//DefaultInfrastructure(this);
 	}
 
 	@Override
 	public Population getInstance(String id, Geography soilGeo, World parent,
-			PlaceLocator loc, EntityInstance owner,int blockStartX, int blockStartZ) {
-		return new SimpleDistrict(id,soilGeo,parent,loc,owner,blockStartX,blockStartZ);
+			PlaceLocator loc, EntityInstance owner,int blockStartX, int blockStartZ,int centerX, int centerZ) {
+		return new SimpleDistrict(id,soilGeo,parent,loc,owner,blockStartX,blockStartZ,centerX,centerZ);
 	}
 
 }
