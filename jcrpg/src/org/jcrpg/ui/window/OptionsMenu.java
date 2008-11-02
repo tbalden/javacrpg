96d45b8c853384a68a8663e20f3c2eee40153aa2
goq669 <goq669@d65a9143-dc31-0410-b20e-ae5b3973b0f5>
2008-11-02 15:23:26 +0000

    two pages working

--- jcrpg/src/org/jcrpg/ui/window/OptionsMenu.java
+++ jcrpg/src/org/jcrpg/ui/window/OptionsMenu.java
@@ -43,6 +43,8 @@ import com.jme.scene.shape.Quad;
  */
 public class OptionsMenu extends PagedInputWindow {
 
+    static final String BG_IMAGE = "./data/ui/baseWindowFrame.dds";
+
     Node pageFirst = new Node();
     Node pageSecond = new Node();
 
@@ -52,23 +54,34 @@ public class OptionsMenu extends PagedInputWindow {
     ValueTuner tunerRenderDistance;
     ValueTuner tunerRenderGrassDistance;
     ValueTuner tunerTextureDetail;
-    TextButton save, cancel;
+    ValueTuner tunerEffectsVolume;
+    ValueTuner tunerMusicVolume;
 
-    public static String[] toggleIds = new String[] {"on","off"};
-    public static String[] toggleTexts = new String[] {Language.v("configuration.on"),Language.v("configuration.off")};
-    public static Object[] toggleObjects = new Object[] {true,false};
+    CheckBox toggleNormalMapShader;
+    ListSelect selectWaterDeatil;
+    static final String[] waterDeatilIds = {Language.v("optionsmenu.on"),
+                                            Language.v("optionsmenu.off"),
+                                            Language.v("optionsmenu.detailed")};
+    CheckBox toggleBloom;
+    CheckBox toggleDepthOfField;
+    ValueTuner tunerShadowDistance;
+    CheckBox toggleSlowAnimation;
+
+    TextButton save, cancel, nextPage, prevPage;
 
     public OptionsMenu(UIBase base) {
         super(base);
         try {
+            // --- First Page ---
+
             // background
-            Quad hudQuad = loadImageToQuad("./data/ui/baseWindowFrame.dds", 
+            Quad hudQuad = loadImageToQuad(BG_IMAGE, 
                                             0.8f*core.getDisplay().getWidth(), 
                                             1.61f*(core.getDisplay().getHeight() / 2), 
                                             core.getDisplay().getWidth() / 2, 
                                             1.13f*core.getDisplay().getHeight() / 2);
             hudQuad.setRenderState(base.hud.hudAS);
-            windowNode.attachChild(hudQuad);
+            pageFirst.attachChild(hudQuad);
 
             // header
             float sizeX = 1.28f* 1.2f * core.getDisplay().getWidth() / 5f;
@@ -77,29 +90,25 @@ public class OptionsMenu extends PagedInputWindow {
             float posX = core.getDisplay().getWidth() / 2;
             Quad header = loadImageToQuad("./data/ui/mainmenu/"+MainMenu.OPTIONS, sizeX, sizeY, posX, posY);
             header.setRenderState(base.hud.hudAS);
-            windowNode.attachChild(header);
+            pageFirst.attachChild(header);
 
-            // --- First Page ---
-            SharedMesh sQuad = new SharedMesh("--",hudQuad);
-            pageFirst.attachChild(sQuad);
-
-            SimpleLayout inputsLayout = new SimpleLayout(0.2f, 0.2f, 0.3f, 0.07f ,2);
+            SimpleLayout inputsLayout = new SimpleLayout(0.2f, 0.16f, 0.3f, 0.07f ,2);
             inputsLayout.addToColumn(0, new TextLabel("",this,pageFirst, 600f, Language.v("optionsmenu.mouselook"), false));
             toggleMLook = new CheckBox("", this, pageFirst, J3DCore.MOUSELOOK);
             inputsLayout.addToColumn(1, toggleMLook, 0.1f, 0.5f);
             addInput(0, toggleMLook);
-            
             inputsLayout.addToColumn(0, new TextLabel("",this,pageFirst, 600f, Language.v("optionsmenu.continuous.load"), false));
             toggleContinuousLoad = new CheckBox("", this, pageFirst, J3DCore.CONTINUOUS_LOAD);
             inputsLayout.addToColumn(1, toggleContinuousLoad, 0.1f, 0.5f);
             addInput(0, toggleContinuousLoad);
 
             inputsLayout.addToColumn(0, new TextLabel("",this,pageFirst, 600f, Language.v("optionsmenu.view.distance"), false));
-            tunerViewDistance = new ValueTuner("",this,pageFirst, 600f, J3DCore.VIEW_DISTANCE, 10, 60, 5);
+            tunerViewDistance = new ValueTuner("",this,pageFirst, 600f, J3DCore.VIEW_DISTANCE, 10, 60, 2);
             inputsLayout.addToColumn(1, tunerViewDistance, 0.35f, 0.5f);
             addInput(0, tunerViewDistance);
             inputsLayout.addToColumn(0, new TextLabel("",this,pageFirst, 600f, Language.v("optionsmenu.render.distance"), false));
-            tunerRenderDistance = new ValueTuner("",this,pageFirst, 600f, J3DCore.RENDER_DISTANCE, 16, 66, 5);
+            int tunerValue = (J3DCore.RENDER_DISTANCE>J3DCore.VIEW_DISTANCE+6 ? J3DCore.RENDER_DISTANCE : J3DCore.VIEW_DISTANCE+6);
+            tunerRenderDistance = new ValueTuner("",this,pageFirst, 600f, tunerValue, 16, 66, 2);
             inputsLayout.addToColumn(1, tunerRenderDistance, 0.35f, 0.5f);
             addInput(0, tunerRenderDistance);
             inputsLayout.addToColumn(0, new TextLabel("",this,pageFirst, 600f, Language.v("optionsmenu.render.grass.distance"), false));
@@ -110,15 +119,73 @@ public class OptionsMenu extends PagedInputWindow {
             tunerTextureDetail = new ValueTuner("",this,pageFirst, 600f, J3DCore.TEXTURE_QUALITY, 0, 2, 1);
             inputsLayout.addToColumn(1, tunerTextureDetail, 0.35f, 0.5f);
             addInput(0, tunerTextureDetail);
-            
+
+            inputsLayout.addToColumn(0, new TextLabel("",this,pageFirst, 600f, Language.v("optionsmenu.effects.volume"), false));
+            tunerEffectsVolume = new ValueTuner("",this,pageFirst, 600f, J3DCore.EFFECT_VOLUME_PERCENT, 0, 100, 5);
+            inputsLayout.addToColumn(1, tunerEffectsVolume, 0.35f, 0.5f);
+            addInput(0, tunerEffectsVolume);
+            inputsLayout.addToColumn(0, new TextLabel("",this,pageFirst, 600f, Language.v("optionsmenu.music.volume"), false));
+            tunerMusicVolume = new ValueTuner("",this,pageFirst, 600f, J3DCore.MUSIC_VOLUME_PERCENT, 0, 100, 5);
+            inputsLayout.addToColumn(1, tunerMusicVolume, 0.35f, 0.5f);
+            addInput(0, tunerMusicVolume);
 
             // buttons
-            save = new TextButton("save",this, pageFirst, 0.3f, 0.75f, 0.18f, 0.06f, 500f,Language.v("behaviorWindow.save"),"S");
+            nextPage = new TextButton("nextPage",this, pageFirst, 0.26f, 0.75f, 0.18f, 0.06f, 500f,Language.v("optionsmenu.next.page"),"N");
+            addInput(0, nextPage);
+            save = new TextButton("save",this, pageFirst, 0.5f, 0.75f, 0.18f, 0.06f, 500f,Language.v("behaviorWindow.save"),"S");
             addInput(0, save);
-            cancel = new TextButton("cancel",this, pageFirst, 0.72f, 0.75f, 0.18f, 0.06f, 500f,Language.v("behaviorWindow.cancel"),"C");
+            cancel = new TextButton("cancel",this, pageFirst, 0.75f, 0.75f, 0.18f, 0.06f, 500f,Language.v("behaviorWindow.cancel"),"C");
             addInput(0, cancel);
 
+            // --- Second Page ---
+
+            // background
+            Quad hudQuad2 = loadImageToQuad(BG_IMAGE, 
+                                            0.8f*core.getDisplay().getWidth(), 
+                                            1.61f*(core.getDisplay().getHeight() / 2), 
+                                            core.getDisplay().getWidth() / 2, 
+                                            1.13f*core.getDisplay().getHeight() / 2);
+            hudQuad2.setRenderState(base.hud.hudAS);
+            pageSecond.attachChild(hudQuad2);
+
+            // header
+            Quad header2 = loadImageToQuad("./data/ui/mainmenu/"+MainMenu.OPTIONS, sizeX, sizeY, posX, posY);
+            header2.setRenderState(base.hud.hudAS);
+            pageSecond.attachChild(header2);
+
+            SimpleLayout inputsLayout2 = new SimpleLayout(0.2f, 0.16f, 0.3f, 0.07f ,2);
+            inputsLayout2.addToColumn(0, new TextLabel("",this,pageSecond, 600f, Language.v("optionsmenu.normalmap.shader"), false));
+            toggleNormalMapShader = new CheckBox("", this, pageSecond, J3DCore.NORMALMAP_ENABLED);
+            inputsLayout2.addToColumn(1, toggleNormalMapShader, 0.1f, 0.5f);
+            addInput(1, toggleNormalMapShader);
+            inputsLayout2.addToColumn(0, new TextLabel("",this,pageSecond, 600f, Language.v("optionsmenu.water.detail"), false));
+            selectWaterDeatil = new ListSelect("water", this, pageSecond, 600f, waterDeatilIds, waterDeatilIds, null, null);
+            inputsLayout2.addToColumn(1, selectWaterDeatil, 0.35f, 0.5f);
+            addInput(1, selectWaterDeatil);
+            inputsLayout2.addToColumn(0, new TextLabel("",this,pageSecond, 600f, Language.v("optionsmenu.bloom"), false));
+            toggleBloom = new CheckBox("", this, pageSecond, J3DCore.BLOOM_EFFECT);
+            inputsLayout2.addToColumn(1, toggleBloom, 0.1f, 0.5f);
+            addInput(1, toggleBloom);
+            inputsLayout2.addToColumn(0, new TextLabel("",this,pageSecond, 600f, Language.v("optionsmenu.dof.effect"), false));
+            toggleDepthOfField = new CheckBox("", this, pageSecond, J3DCore.DOF_EFFECT);
+            inputsLayout2.addToColumn(1, toggleDepthOfField, 0.1f, 0.5f);
+            addInput(1, toggleDepthOfField);
+            inputsLayout2.addToColumn(0, new TextLabel("",this,pageSecond, 600f, Language.v("optionsmenu.shadow.distance"), false));
+            tunerShadowDistance = new ValueTuner("",this,pageSecond, 600f, J3DCore.RENDER_SHADOW_DISTANCE, 0, 20, 1);
+            inputsLayout2.addToColumn(1, tunerShadowDistance, 0.35f, 0.5f);
+            addInput(1, tunerShadowDistance);
+            inputsLayout2.addToColumn(0, new TextLabel("",this,pageSecond, 600f, Language.v("optionsmenu.slow.animation"), false));
+            toggleSlowAnimation = new CheckBox("", this, pageSecond, J3DCore.SLOW_ANIMATION);
+            inputsLayout2.addToColumn(1, toggleSlowAnimation, 0.1f, 0.5f);
+            addInput(1, toggleSlowAnimation);
+
+            // buttons
+            prevPage = new TextButton("prevPage",this, pageSecond, 0.26f, 0.75f, 0.18f, 0.06f, 500f,Language.v("optionsmenu.prev.page"),"P");
+            addInput(1, prevPage);
+
+            // adding pages
             addPage(0, pageFirst);
+            addPage(1, pageSecond);
             windowNode.updateRenderState();
             base.addEventHandler("back", this);
         } catch (Exception ex) {
@@ -130,33 +197,102 @@ public class OptionsMenu extends PagedInputWindow {
     public boolean handleKey(String key) {
         if (super.handleKey(key)) return true;
         if (key.equals("back")) {
-            toggle();
-            core.mainMenu.toggle();
+            if (currentPage>0) {
+                currentPage=0;
+                setupPage();
+                changePage(0);
+            } else {
+                toggle();
+                core.mainMenu.toggle();
+            }
         }
         return true;
     }
 
     @Override
     public boolean inputUsed(InputBase base, String message) {
+        // Save
         if (base == save) {
+            // Setting new Values to J3DCore
+            J3DCore.MOUSELOOK = toggleMLook.isChecked();
+            J3DCore.CONTINUOUS_LOAD = toggleContinuousLoad.isChecked();
+
+            J3DCore.VIEW_DISTANCE = tunerViewDistance.getSelection();
+            J3DCore.RENDER_DISTANCE = tunerRenderDistance.getSelection();
+            J3DCore.RENDER_GRASS_DISTANCE = tunerRenderGrassDistance.getSelection();
+            J3DCore.TEXTURE_QUALITY = tunerTextureDetail.getSelection();
+            J3DCore.EFFECT_VOLUME_PERCENT = tunerEffectsVolume.getSelection();
+            J3DCore.MUSIC_VOLUME_PERCENT = tunerMusicVolume.getSelection();
+
+            J3DCore.NORMALMAP_ENABLED = toggleNormalMapShader.isChecked();
+            // TODO: selectWaterDeatil value exam
+            //J3DCore.WATER_SHADER;
+            //J3DCore.WATER_DETAILED;
+            J3DCore.BLOOM_EFFECT = toggleBloom.isChecked();
+            J3DCore.DOF_EFFECT = toggleDepthOfField.isChecked();
+            J3DCore.RENDER_SHADOW_DISTANCE = tunerShadowDistance.getSelection();
+            J3DCore.SLOW_ANIMATION = toggleSlowAnimation.isChecked();
+
             // TODO: save to config file
+
+            // back to main menu
             toggle();
             core.mainMenu.toggle();
             return true;
+        // Cancel
         } else if (base == cancel) {
+            // Resetting Ui elements
+            toggleMLook.setChecked(J3DCore.MOUSELOOK);
+            toggleContinuousLoad.setChecked(J3DCore.CONTINUOUS_LOAD);
+
+            tunerViewDistance.setValue(J3DCore.VIEW_DISTANCE);
+            tunerRenderDistance.setValue(J3DCore.RENDER_DISTANCE);
+            tunerRenderGrassDistance.setValue(J3DCore.RENDER_GRASS_DISTANCE);
+            tunerTextureDetail.setValue(J3DCore.TEXTURE_QUALITY);
+            tunerEffectsVolume.setValue(J3DCore.EFFECT_VOLUME_PERCENT);
+            tunerMusicVolume.setValue(J3DCore.MUSIC_VOLUME_PERCENT);
+
+            toggleNormalMapShader.setChecked(J3DCore.NORMALMAP_ENABLED);
+            toggleNormalMapShader.setChecked(J3DCore.NORMALMAP_ENABLED);
+            // TODO: selectWaterDeatil value exam
+            toggleBloom.setChecked(J3DCore.BLOOM_EFFECT);
+            toggleDepthOfField.setChecked(J3DCore.DOF_EFFECT);
+            tunerShadowDistance.setValue(J3DCore.RENDER_SHADOW_DISTANCE);
+            toggleSlowAnimation.setChecked(J3DCore.SLOW_ANIMATION);
+
+            // back to main menu
             toggle();
             core.mainMenu.toggle();
             return true; 
-        } else if (base.equals(tunerViewDistance)) {
-            if (message.equals("enter")) {
-                System.out.println("--- tunerViewDistance: "+message);
-            } else if (message.equals("lookLeft") || message.equals("lookRight")) {
-                tunerViewDistance.setUpdated(true);
-                System.out.println("--- tunerViewDistance: "+message+", tunerViewDistance.value"+tunerViewDistance.getSelection());
-            } 
+        // Next Page
+        } else if (base == nextPage) {
+            currentPage=1;
+            nextPage.deactivate();
+            setupPage();
+            return true; 
+        // Previous Page
+        } else if (base == prevPage) {
+            currentPage=0;
+            prevPage.deactivate();
+            setupPage();
+            return true; 
+        } else if (base == tunerViewDistance) {
+            System.out.println("tunerViewDistance: "+tunerViewDistance.getSelection()+", tunerRenderDistance: "+tunerRenderDistance.getSelection());
+            if (tunerViewDistance.getSelection()+6>tunerRenderDistance.getSelection()) {
+                tunerRenderDistance.setValue(tunerViewDistance.getSelection()+6);
+                tunerRenderDistance.setUpdated(true);
+                tunerRenderDistance.deactivate();
+            }
+        } else if (base == tunerRenderDistance) {
+            System.out.println("tunerViewDistance: "+tunerViewDistance.getSelection()+", tunerRenderDistance: "+tunerRenderDistance.getSelection());
+            if (tunerViewDistance.getSelection()+6>tunerRenderDistance.getSelection()) {
+                tunerRenderDistance.setValue(tunerViewDistance.getSelection()+6);
+                tunerRenderDistance.setUpdated(true);
+                tunerRenderDistance.activate();
+            }
         }
 
-        return false;
+        return true;
     }
 
 }
