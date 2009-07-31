9d4ba3e752474aa0d7c8d30e4a4a3e63574d4e01
Pál Zoltán Illés <1277993+tbalden@users.noreply.github.com>
2009-07-31 22:03:02 +0000

    roadnetwork dev 2

--- jcrpg/src/org/jcrpg/world/place/economic/Town.java
+++ jcrpg/src/org/jcrpg/world/place/economic/Town.java
@@ -2,6 +2,8 @@ package org.jcrpg.world.place.economic;
 
 import java.util.ArrayList;
 
+import org.jcrpg.threed.J3DCore;
+
 /**
  * The meta class for joined neighbor districts.
  * @author pali
@@ -11,6 +13,57 @@ public class Town {
 	
 	public ArrayList<Population> subPopulations = new ArrayList<Population>();
 	public String foundationName = null;
+	
+	
+	int[] retVal = null;
+
+	/**
+	 * Town size.
+	 * @return Town size - no. of subpopulations
+	 */
+	public int getSize()
+	{
+		return subPopulations.size();
+	}
+	
+	/**
+	 * x, z
+	 * @return
+	 */
+	public int[] getCenterCoordinates()
+	{
+		if (retVal!=null)
+		{
+			return retVal;
+		} else
+		{
+			update();
+		}
+		return retVal; 
+	}
+	
+	public void update()
+	{
+		long lcenterX = 0 , lcenterZ = 0;
+		
+		if (subPopulations.size()==0)
+		{
+			retVal = null;
+			return;
+		}
+		retVal = new int[2];
+		
+		for (Population p:subPopulations)
+		{
+			lcenterX+=p.centerX;
+			lcenterZ+=p.centerZ;
+		}
+		lcenterX/=subPopulations.size();
+		lcenterZ/=subPopulations.size();
+		retVal[0] = (int)lcenterX;
+		retVal[1] = (int)lcenterZ;
+		
+	}
 	
 
 }
