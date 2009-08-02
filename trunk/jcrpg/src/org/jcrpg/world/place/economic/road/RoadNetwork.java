9d022cb7ec70dd799df62fcd52fcec2304c88d9a
skrionius <skrionius@d65a9143-dc31-0410-b20e-ae5b3973b0f5>
2009-08-02 21:31:45 +0000

    mkienenb - add message Keyboard in menus, road dev 3

--- trunk/jcrpg/src/org/jcrpg/world/place/economic/road/RoadNetwork.java
+++ trunk/jcrpg/src/org/jcrpg/world/place/economic/road/RoadNetwork.java
@@ -4,13 +4,28 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.TreeMap;
 
+import org.jcrpg.audio.AudioServer;
+import org.jcrpg.space.Cube;
+import org.jcrpg.space.Side;
+import org.jcrpg.space.sidetype.Climbing;
+import org.jcrpg.space.sidetype.GroundSubType;
+import org.jcrpg.space.sidetype.SideSubType;
 import org.jcrpg.threed.J3DCore;
+import org.jcrpg.world.ai.DistanceBasedBoundary;
+import org.jcrpg.world.ai.EntityInstance;
+import org.jcrpg.world.place.BoundaryUtils;
 import org.jcrpg.world.place.Economic;
 import org.jcrpg.world.place.EconomyContainer;
 import org.jcrpg.world.place.FlowGeography;
+import org.jcrpg.world.place.Geography;
+import org.jcrpg.world.place.Place;
+import org.jcrpg.world.place.PlaceLocator;
+import org.jcrpg.world.place.Water;
 import org.jcrpg.world.place.World;
+import org.jcrpg.world.place.WorldSizeBitBoundaries;
 import org.jcrpg.world.place.WorldSizeFlowDirections;
 import org.jcrpg.world.place.economic.Town;
+import org.jcrpg.world.place.water.Ocean;
 
 import com.jme.math.Vector2f;
 
@@ -30,7 +45,12 @@ public class RoadNetwork extends Economic implements FlowGeography {
 		this.container = container;
 		magnification = blockSize;
 		flowDirections = new WorldSizeFlowDirections(magnification,(World)parent);
+		setBoundaries(new WorldSizeBitBoundaries(magnification,(World)parent));
 	}
+	
+	// TODO SoilGeo for each block get it independently!
+	// override EcoGround method original coding
+	// EconomicContainer - use roadNetwork for getCube
 	
 	public class Connection 
 	{
@@ -217,6 +237,7 @@ public class RoadNetwork extends Economic implements FlowGeography {
 					try {
 						System.out.println(x+" - "+z);
 						getWorldSizeFlowDirections().setCubeFlowDirection(x, worldGroundLevel, z, J3DCore.WEST, true); 
+						getBoundaries().addCube(magnification, x, worldGroundLevel, z);
 					} catch (Exception ex)
 					{
 						ex.printStackTrace();
@@ -232,6 +253,7 @@ public class RoadNetwork extends Economic implements FlowGeography {
 							System.out.println(x+" | "+z);
 							getWorldSizeFlowDirections().setCubeFlowDirection(x, worldGroundLevel, z, J3DCore.NORTH, true);
 							getWorldSizeFlowDirections().setCubeFlowDirection(x, worldGroundLevel, z, J3DCore.WEST, true);
+							getBoundaries().addCube(magnification, x, worldGroundLevel, z);
 						} catch (Exception ex)
 						{
 							ex.printStackTrace();
@@ -257,7 +279,8 @@ public class RoadNetwork extends Economic implements FlowGeography {
 					// draw Z+1
 					try {
 						System.out.println(x+" || "+z);
-						getWorldSizeFlowDirections().setCubeFlowDirection(x, worldGroundLevel, z, J3DCore.NORTH, true); 
+						getWorldSizeFlowDirections().setCubeFlowDirection(x, worldGroundLevel, z, J3DCore.NORTH, true);
+						getBoundaries().addCube(magnification, x, worldGroundLevel, z);
 					} catch (Exception ex)
 					{
 						ex.printStackTrace();
@@ -273,6 +296,7 @@ public class RoadNetwork extends Economic implements FlowGeography {
 							System.out.println(x+" -- "+z);
 							getWorldSizeFlowDirections().setCubeFlowDirection(x, worldGroundLevel, z, J3DCore.NORTH, true);
 							getWorldSizeFlowDirections().setCubeFlowDirection(x, worldGroundLevel, z, J3DCore.WEST, true);
+							getBoundaries().addCube(magnification, x, worldGroundLevel, z);
 						} catch (Exception ex)
 						{
 							ex.printStackTrace();
@@ -297,6 +321,117 @@ public class RoadNetwork extends Economic implements FlowGeography {
 		return flowDirections;
 	}
 	
+	public static final String TYPE_ECOGROUND = "ECOGROUND";
+	
+	public static final SideSubType SUBTYPE_STAIRS = new Climbing(TYPE_ECOGROUND+"_STAIRS",true);
+	public static final SideSubType SUBTYPE_STREETGROUND = new GroundSubType(TYPE_ECOGROUND+"_STREETGROUND",false);
+	public static final SideSubType SUBTYPE_EXTERNAL_WOODEN_GROUND = new GroundSubType(TYPE_ECOGROUND+"_EXTERNAL_GROUND",true);
+	
+	static
+	{
+		SUBTYPE_STREETGROUND.continuousSoundType = "town_street_easy";
+		SUBTYPE_STREETGROUND.audioStepType = AudioServer.STEP_STONE;
+		SUBTYPE_STREETGROUND.colorOverwrite = true;
+		SUBTYPE_STREETGROUND.colorBytes = new byte[] {(byte)150,(byte)150,(byte)150};
+		SUBTYPE_STAIRS.audioStepType = AudioServer.STEP_STONE;
+		SUBTYPE_STAIRS.colorOverwrite = true;
+		SUBTYPE_STAIRS.colorBytes = new byte[] {(byte)150,(byte)50,(byte)50};
+	}
+	public static Side[] STAIRS = new Side[]{new Side(TYPE_ECOGROUND,SUBTYPE_STAIRS)};
+	public static Side[] ECOGROUND = new Side[]{new Side(TYPE_ECOGROUND,SUBTYPE_STREETGROUND)};
+	
+
+	static Side[][] STEPS_NORTH = new Side[][] { STAIRS, I_EMPTY, INTERNAL_ROCK_SIDE,I_EMPTY,BLOCK, GROUND};
+	static Side[][] STEPS_SOUTH = new Side[][] { INTERNAL_ROCK_SIDE, I_EMPTY, STAIRS,I_EMPTY,BLOCK,GROUND};
+	static Side[][] STEPS_WEST = new Side[][] { I_EMPTY, INTERNAL_ROCK_SIDE, I_EMPTY,STAIRS,BLOCK,GROUND};
+	static Side[][] STEPS_EAST = new Side[][] { I_EMPTY, STAIRS, I_EMPTY,INTERNAL_ROCK_SIDE,BLOCK,GROUND};
+
+	public static Side[][] EXTERNAL = new Side[][] { null, null, null,null,null,ECOGROUND };
+	public static Side[][] EXTERNAL_WATER_WOODEN_GROUND = new Side[][] { null, null, null,null,null,{Ocean.SHALLOW_WATER_SIDE,new Side(TYPE_ECOGROUND,SUBTYPE_EXTERNAL_WOODEN_GROUND)}};
+
+	
+	
+	public static HashMap<Integer, Cube> hmKindCubeOverride = new HashMap<Integer, Cube>();
+	public static HashMap<Integer, Cube> hmKindCubeOverride_FARVIEW = new HashMap<Integer, Cube>();
+	
+	static 
+	{
+		Cube ground = new Cube(null,EXTERNAL,0,0,0,true,false);
+		hmKindCubeOverride.put(K_NORMAL_GROUND, ground);
+		Cube waterGround = new Cube(null,EXTERNAL_WATER_WOODEN_GROUND,0,0,0,true,false);
+		hmKindCubeOverride.put(K_WATER_GROUND, waterGround );		
+		Cube stepsEast = new Cube(null,STEPS_EAST,0,0,0,true,true);
+		hmKindCubeOverride.put(K_STEEP_EAST, stepsEast);
+		Cube stepWest = new Cube(null,STEPS_WEST,0,0,0,true,true);
+		hmKindCubeOverride.put(K_STEEP_WEST, stepWest);
+		Cube stepNorth = new Cube(null,STEPS_NORTH,0,0,0,true,true);
+		hmKindCubeOverride.put(K_STEEP_NORTH, stepNorth);
+		Cube stepSouth = new Cube(null,STEPS_SOUTH,0,0,0,true,true);
+		hmKindCubeOverride.put(K_STEEP_SOUTH, stepSouth);
+
+		//hmKindCubeOverride_FARVIEW.put(K_NORMAL_GROUND, new Cube(null,House.EXTERNAL,0,0,0));
+	}
+	
+	public HashMap<Integer, Cube> getOverrideMap()
+	{
+		return hmKindCubeOverride;
+	}
+
+	@Override
+	public Cube getCubeObject(int kind, int worldX, int worldY, int worldZ, boolean farView) {
+		Cube c = farView?hmKindCubeOverride_FARVIEW.get(kind):getOverrideMap().get(kind);
+		return c;
+	}
+
+	
+	
+	@Override
+	public boolean overrideGeoHeight() {
+		return false;
+	}
+
+	
+	
+	@Override
+	public float[] getCubeKind(long key, int worldX, int worldY, int worldZ, boolean farView) {
+		// let's check for waters here...
+		boolean water = false;
+		for (Water geo:((World)getRoot()).waters.values())
+		{
+			{
+				if (geo.getBoundaries().isInside(worldX, geo.worldGroundLevel, worldZ))
+				{
+					if (geo.isWaterPoint(worldX, geo.worldGroundLevel, worldZ, farView)) water = true;
+				}
+			}
+		}
+		float[] retKind = null;
+		if (soilGeo!=null && soilGeo.getBoundaries().isInside(worldX, worldY, worldZ))
+		{
+			retKind = soilGeo.getCubeKind(key, worldX,worldY,worldZ, farView);
+		} else
+		{
+			retKind = super.getCubeKindOutside(key, worldX, worldY, worldZ, farView);
+		}
+		if (water && retKind[4]==K_NORMAL_GROUND)
+		{
+			retKind[4] = K_WATER_GROUND;
+		}
+		return retKind;
+		
+	}
+
+	@Override
+	protected float getPointHeightInside(int x, int z, int sizeX, int sizeZ, int worldX, int worldZ, boolean farView) {
+		// use the height defined by the geography here...
+		if (soilGeo.getBoundaries().isInside(worldX, soilGeo.worldGroundLevel, worldZ))
+		{
+			int[] values = soilGeo.calculateTransformedCoordinates(worldX, soilGeo.worldGroundLevel, worldZ);
+			return soilGeo.getPointHeight(values[3], values[5], values[0], values[2],worldX,worldZ, farView);
+		}
+		return getPointHeightOutside(worldX, worldZ, farView);
+	}
+
 	
 	
 
